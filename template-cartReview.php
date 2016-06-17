cc356672210e0f141aaf0ef71e398cff9b76f36d
locher <loic@herrloc.fr>
2016-06-17 14:11:08 +0200

    Email + pont après commande

---
+++ template-cartReview.php
@@ -0,0 +1,41 @@
+<?php /* Template Name: Cart review Template */ get_header(); ?>
+	
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+	<main role="main">
+
+		<section class="big-header commun-header">
+			<div class="wrapperBigHeader">
+				<?php the_field('contenu_header'); ?>
+			</div>
+			
+			<?php if(have_rows('images_de_fond')): ?>
+			
+			<div class="bigHeader-fond" aria-hidden="true">
+			
+				<?php while ( have_rows('images_de_fond') ) : the_row();?>
+					<?php $image = get_sub_field('image_de_fond'); ?>
+					<div style="background-image: url('<?php echo $image['sizes']['bigHeaderimg']; ?>');"></div>
+				<?php endwhile; ?>
+				
+			</div>
+			
+			<?php endif;?>
+			
+		</section>
+		
+		<div class="content-wrapper">
+			
+			<div class="content review_template">
+			
+				<?php the_content();?>
+				
+			</div>
+			
+		</div>
+	
+	</main>
+	
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
