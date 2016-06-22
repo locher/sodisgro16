90ee9158fca8acc1825ec0be451b8852bffebbce
locher <loic@herrloc.fr>
2016-06-22 16:36:41 +0200

    Page 404

--- 404.php
+++ 404.php
@@ -1,24 +1,21 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
-
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
+	
+		<section class="big-header commun-header">
+			<div class="wrapperBigHeader">
+				<h1>Erreur 404</h1>
+				<p>Désolé, mais la page n'existe pas ou n'existe plus :(</p>
+				
+				<a class="bt" href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+			</div>
+			
+			<div class="bigHeader-fond" aria-hidden="true"></div>
+			
+		
+			
 		</section>
-		<!-- /section -->
+		<!-- section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
